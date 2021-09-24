5fea69feecbe6e935c2b362d65c797d8f86e3c77
Muya Chang <muya.chang.0121@gmail.com>
2021-09-23 20:37:57 -0400

    Add "__name__ == __main__" for main function

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from PyTerminal import PyTerminal
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+0
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    PT = PyTerminal()
 
-PT = PyTerminal()
-
-print('<< TC Terminal >>')
-while 1 :
-    command = input("ICSRL> ")
-    if command == 'exit':
-        exit()
-    elif command != '':
-        PT.decodeCommand(command)
+    print('<< TC Terminal >>')
+    while 1 :
+        command = input("ICSRL> ")
+        if command == 'exit':
+            exit()
+        elif command != '':
+            PT.decodeCommand(command)
