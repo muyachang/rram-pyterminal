a2b88b6756eaa250a948302e4f7517e281d7f13e
Muya Chang <muya.chang.0121@gmail.com>
2021-09-27 19:07:24 -0400

    Add/Fix "verbal" option for each function

--- Board/TC.py
+++ Board/TC.py
@@ -1,17 +1,16 @@
 from Lib import CommandMap as CM
 
 
-def connect(pyterminal):
-    response = pyterminal.send_command(CM.CM_TC + ' ' + CM.CM_TC_CONNECT, False)
-    print('Testchip ID: ' + response.split()[0])
+def connect(pyterminal, verbal):
+    pyterminal.send_command(CM.CM_TC + ' ' + CM.CM_TC_CONNECT, verbal)
 
 
-def read(pyterminal, address):
-    pyterminal.send_command(CM.CM_TC + ' ' + CM.CM_TC_READ + ' ' + address, True)
+def read(pyterminal, address, verbal):
+    pyterminal.send_command(CM.CM_TC + ' ' + CM.CM_TC_READ + ' ' + address, verbal)
 
 
-def write(pyterminal, address, value):
-    pyterminal.send_command(CM.CM_TC + ' ' + CM.CM_TC_WRITE + ' ' + address + ' ' + value, True)
+def write(pyterminal, address, value, verbal):
+    pyterminal.send_command(CM.CM_TC + ' ' + CM.CM_TC_WRITE + ' ' + address + ' ' + value, verbal)
 
 
 def unknown(parameters):
@@ -19,7 +18,7 @@ def unknown(parameters):
 
 
 def decode(pyterminal, parameters):
-    if   parameters[1] == 'connect': connect(pyterminal)
-    elif parameters[1] == 'read'   : read   (pyterminal, parameters[2])
-    elif parameters[1] == 'write'  : write  (pyterminal, parameters[2], parameters[3])
+    if   parameters[1] == 'connect': connect(pyterminal, True)
+    elif parameters[1] == 'read'   : read   (pyterminal, parameters[2], True)
+    elif parameters[1] == 'write'  : write  (pyterminal, parameters[2], parameters[3], True)
     else: unknown(parameters)
