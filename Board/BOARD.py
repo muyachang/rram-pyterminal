59e3767299cc60c5ec6914165cbcdb8978f3a7ea
Muya Chang <muya.chang.0121@gmail.com>
2021-11-03 19:20:58 -0400

    Add verbal parameter for version function

--- Board/BOARD.py
+++ Board/BOARD.py
@@ -1,14 +1,15 @@
 from Lib import CommandMap as CM
 
 
-def version(pyterminal):
-    pyterminal.send_command(CM.CM_BOARD + ' ' + CM.CM_BOARD_VERSION, True)
+def version(pyterminal, verbal):
+    return pyterminal.send_command(CM.CM_BOARD + ' ' + CM.CM_BOARD_VERSION, verbal)
+
 
 def unknown(parameters):
     print('Unknown Command: ' + ' '.join(parameters) + '(From PyTerminal)')
 
 
 def decode(pyterminal, parameters):
-    if   parameters[1] == 'version': version(pyterminal)
+    if   parameters[1] == 'version': version(pyterminal, True)
     else: unknown(parameters)
         
